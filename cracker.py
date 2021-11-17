eec74d2a7b90a87b0e4415297b64299947bd194b
Munseer-am <82161602+Munseer-am@users.noreply.github.com>
2021-11-17 17:19:18 +0530

    Update cracker.py

--- cracker.py
+++ cracker.py
@@ -1,11 +1,6 @@
 import hashlib
 
-def Passwd_Cracker():
-        	print("""  \033[1;34;40m__  __
- |  \/  |_   _ _ __  ___  ___  ___ _ __
- | |\/| | | | | '_ \/ __|/ _ \/ _ \ '__|
- | |  | | |_| | | | \__ \  __/  __/ |
- |_|  |_|\__,_|_| |_|___/\___|\___|_|""")
+def Passwd_Cracker():	
 	hash = input('Enter md5 hash: ')
 	f = open('rockyou.txt','rb')
 	str = f.read()
